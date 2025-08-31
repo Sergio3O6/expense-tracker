ab05c1862d13f93fcd4aaf1f82fc3e4cf07eaa8e
Sergio3O6 <24smedrano@gmail.com>
2025-08-30 23:04:00 -0500

    Added edit expense

---
+++ app.py
@@ -0,0 +1,105 @@
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+
+app = Flask(__name__)
+
+#Creates database if one does not exists
+def init_db():
+    conn = sqlite3.connect('expenses.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS expenses
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  description TEXT,
+                  amount REAL,
+                  category TEXT,
+                  date TEXT)''')
+    conn.commit()
+    conn.close()
+
+#Homepage for all the expenses to be shown
+@app.route('/')
+def index():
+    conn = sqlite3.connect('expenses.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM expenses")
+    expenses = c.fetchall()
+    conn.close()
+    return render_template('index.html', expenses=expenses)
+
+#Adds new expense to table
+@app.route('/add', methods=['GET', 'POST'])
+def add_expense():
+    if request.method == 'POST':
+        description = request.form['description']
+        amount = float(request.form['amount'])
+        category = request.form['category']
+        date = request.form['date']
+
+        conn = sqlite3.connect('expenses.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO expenses (description, amount, category, date) VALUES (?, ?, ?, ?)",
+                  (description, amount, category, date))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('index'))
+    return render_template('add_expense.html')
+
+#Deletes an expense
+@app.route('/delete/<int:expense_id>')
+def del_expense(expense_id):
+        conn = sqlite3.connect('expenses.db')
+        c = conn.cursor()
+        c.execute("DELETE FROM expenses WHERE id =?", (expense_id,))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('delete_page'))
+
+#Page to show all expenses with a delete button
+@app.route('/delete_page')
+def delete_page():
+    conn = sqlite3.connect('expenses.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM expenses")
+    expenses = c.fetchall()
+    conn.close()
+    return render_template('delete_expense.html', expenses=expenses)
+
+#Page to show all expenses with an edit button
+@app.route('/edit_page')
+def edit_page():
+    conn = sqlite3.connect("expenses.db")
+    c = conn.cursor()
+    c.execute('SELECT * FROM expenses')
+    expenses = c.fetchall()
+    conn.close()
+    return render_template('edit_expense.html', expenses=expenses)\
+
+#Edit a single expense
+@app.route('/edit/<int:expense_id>', methods=["GET", "POST"])
+def edit_expense(expense_id):
+    conn = sqlite3.connect('expenses.db')
+    c = conn.cursor()
+    
+    if request.method == "POST":
+        description = request.form["description"]
+        amount = float(request.form["amount"])
+        category = request.form["category"]
+        date = request.form["date"]
+
+        c.execute('''
+            UPDATE expenses
+            SET description=?, amount=?, category=?, date=?
+            WHERE id=?
+        ''', (description, amount, category, date, expense_id))
+        conn.commit()
+        conn.close()
+        return redirect(url_for("edit_page"))
+    
+    c.execute("SELECT * FROM expenses WHERE id=?", (expense_id,))
+    expense = c.fetchone()
+    conn.close()
+    return render_template("edit_single.html", expense=expense)
+
+if __name__ == '__main__':
+    init_db()
+    app.run(debug=True)
